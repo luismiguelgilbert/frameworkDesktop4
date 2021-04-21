67e6d11c8f55ce27441abdef334885456e49fef3
Luis Miguel Gilbert <luismiguelgilbert@gmail.com>
2021-04-21 15:19:57 -0500

    5.0.7

--- src/pages/RootENS/ENSEquipista/edit.vue
+++ src/pages/RootENS/ENSEquipista/edit.vue
@@ -162,7 +162,7 @@ export default ({
                 { name: 'address', filterBy: null, fields: ['countryID','cityID', 'addressName', 'tipo'] },
                 { name: 'mails', filterBy: null, fields: ['mailAddress'] },
                 { name: 'children', filterBy: null, fields: ['childrenName','sexo', 'birthday'] },
-                { name: 'servicios', filterBy: null, fields: ['servicioID','fechaInicio', 'comments'] },
+                { name: 'servicios', filterBy: null, fields: ['servicioID','fechaInicio', 'fechaSalida', 'comments'] },
                 { name: 'jobs', filterBy: null, fields: ['companyName','jobPosition', 'jobStartDate','jobStopDate','jobSkills','jobPlaces'] }
             ],
             //hide Tabs if something happens
