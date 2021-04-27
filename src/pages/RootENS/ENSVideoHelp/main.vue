3ff679134cb032d6802d9584d39b5c670a805890
Luis Miguel Gilbert <luismiguelgilbert@gmail.com>
2021-04-27 12:23:07 -0500

    5.0.8

--- src/pages/RootENS/ENSVideoHelp/main.vue
+++ src/pages/RootENS/ENSVideoHelp/main.vue
@@ -1,3 +1,28 @@
 <template>
-    <q-video src="https://www.youtube.com/embed/_FDtTrHlCgc?autoplay=1" style="height: calc(100vh - 50px);" />
+<div>
+    <q-dialog v-model="seamless">
+        <q-card class="no-padding" style="height: 315px; width: 560px;">
+            <q-video :ratio="16/9" src="https://www.youtube.com/embed/_FDtTrHlCgc?autoplay=1" />
+        </q-card>
+        <!--<q-card>
+            <q-video src="https://youtu.be/boEBmfjsNFg"/>
+        </q-card>-->
+    </q-dialog>
+
+    
+</div>
 </template>
+<script>
+
+export default ({
+    data () {
+        return {
+            //Confirmed
+            router: this.$router,
+            seamless: true,
+        }
+    },
+    
+})
+
+</script>
